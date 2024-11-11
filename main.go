730263e130d61efcb2f47b50ddd67ae9ccda8b53
Алексей Кузьмин <akuzmin340@gmail.com>
2024-11-11 09:32:03 +0200

    -

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"errors"
-	"fmt"
+	"github.com/geziyor/geziyor"
+	"github.com/geziyor/geziyor/client"
 	_ "github.com/go-sql-driver/mysql"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"slices"
-	"strconv"
+	"os"
 	"time"
-	"unicode"
 )
 
 const (
@@ -19,95 +19,72 @@ const (
 )
 
 var (
-	urls = map[string]string{
-		"all":           "https://kaliningrad.hh.ru/search/vacancy?ored_clusters=true&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&text=golang",
-		"kld":           "https://kaliningrad.hh.ru/search/vacancy?text=golang&salary=&ored_clusters=true&area=41&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line",
-		"no_experience": "https://kaliningrad.hh.ru/search/vacancy?text=golang&salary=&ored_clusters=true&experience=noExperience&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line",
-		"remote":        "https://kaliningrad.hh.ru/search/vacancy?text=golang&salary=&schedule=remote&ored_clusters=true&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line",
+	urls = []string{
+		"https://spb.hh.ru/search/vacancy?hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&text=golang&enable_snippets=false&L_save_area=true",
+		"https://spb.hh.ru/search/vacancy?hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&L_save_area=true&area=41&text=golang",
+		"https://spb.hh.ru/search/vacancy?hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&L_save_area=true&experience=noExperience&text=golang",
+		"https://spb.hh.ru/search/vacancy?hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&L_save_area=true&schedule=remote&text=golang",
 	}
+
+	urlNames = map[string]string{
+		urls[0]: "all",
+		urls[1]: "kld",
+		urls[2]: "no_experience",
+		urls[3]: "remote",
+	}
+
+	date string
+
 	search = []byte("<h1 data-qa=\"bloko-header-3\" class=\"bloko-header-section-3\">")
 )
 
+var db *sql.DB
+
+func init() {
+	var err error
+
+	f, err := os.ReadFile("db_conn.txt")
+	if err != nil {
+		panic("opening db_conn.txt:" + err.Error())
+	}
+
+	db, err = sql.Open(DBDriver, string(f))
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	defer db.Close()
+
+	//simpleGet()
+
 	for {
-		go Start()
+		date = time.Now().Format(time.DateOnly)
+
+		log.Println(date)
+
+		go func() {
+			geziyor.NewGeziyor(&geziyor.Options{
+				StartURLs: urls,
+				ParseFunc: ParseFunc,
+			}).Start()
+		}()
+
 		time.Sleep(time.Hour * 24)
 	}
 
 }
 
-func Start() {
-	date := time.Now().Format(time.DateOnly)
-
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("recover: ", r)
-		} else {
-			fmt.Println("ok", date)
-		}
-	}()
-
-	db, err := sql.Open(DBDriver, DBSource)
-	if err != nil {
-		panic(err)
-	}
-	if err := db.Ping(); err != nil {
-		panic(err)
+func ParseFunc(g *geziyor.Geziyor, r *client.Response) {
+	if err := os.WriteFile(urlNames[r.Request.URL.String()]+".html", r.Body, 600); err != nil {
+		log.Println("writing response to file", err)
 	}
 
-	data := make(map[string]int)
-
-	for key, url := range urls {
-
-		body, err := GetBody(url)
-		if err != nil {
-			panic(err)
-		}
-		num, err := GetNum(body, search)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(num)
-
-		data[key] = num
-
-	}
-
-	if err := WriteData(db, date, data); err != nil {
-		panic(err)
-	}
-}
-
-func GetBody(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
-}
-
-func GetNum(body []byte, search []byte) (int, error) {
-	lenS := len(search)
-
-	for i := lenS; i <= len(body); i++ {
-		if slices.Equal(body[i-lenS:i], search) {
-			res := ""
-			for ; ; i++ {
-				if unicode.IsDigit(rune(body[i])) {
-					res += string(body[i])
-				} else if string(body[i]) == "<" {
-					num, _ := strconv.Atoi(res)
-					return num, nil
-				}
-			}
-		}
-	}
-	return 0, errors.New("not found")
 }
 
 func WriteData(db *sql.DB, date string, data map[string]int) error {
@@ -116,4 +93,65 @@ func WriteData(db *sql.DB, date string, data map[string]int) error {
 		return err
 	}
 	return nil
+}
+
+func simpleGet() {
+	const (
+		Url = "https://spb.hh.ru/search/vacancy?hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&text=golang&enable_snippets=false&L_save_area=true"
+	)
+
+	req, err := http.NewRequest("GET", Url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Println("error code ", resp.StatusCode)
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	ioutil.WriteFile("test.html", b, 0644)
+}
+
+func findNum(s string) int {
+	var days int
+	isFindNum := false
+
+	for _, a := range s {
+		d, ok := digits[a]
+		if ok && !isFindNum {
+			isFindNum = true
+			days = d
+
+		} else if isFindNum {
+			if ok {
+				days = days*10 + d
+			} else {
+				break
+			}
+		}
+	}
+
+	return days
+}
+
+var digits = map[rune]int{
+	'0': 0,
+	'1': 1,
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
 }
